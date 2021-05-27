c400da15565a5b9e5c35b914a786762d9935ad33
Zhantai <Zhantai.Ismailov@gmail.com>
2021-05-27 19:40:50 +0600

    add adding

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -8,3 +8,14 @@ def homepage():
     f = open('good.txt', 'r+', encoding='utf-8')
     txt = f.readlines()
     return render_template('index.html', goods=txt)
+
+
+@app.route('/add/', methods=["POST"])
+def add():
+    good = request.form["good"]
+    f = open('good.txt', 'a+', encoding='utf-8')
+    f.write("\n" + good)
+    f.close()
+    return """<h1>Инвентарь пополнен</h1>
+              <a href='/'> Вернуться обратно </a>
+           """
